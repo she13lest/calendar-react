30e4c77c6b6e68865713e59f0c530689f0569c69
Anna <she13lest@gmail.com>
2022-09-08 19:51:39 +0200

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import "./header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <button className="button create-event-btn" onClick={onOpenModal}>
+      <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
